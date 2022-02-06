ef85e118884a60425216eedbd61dbf118c5b9694
Yanjun Chen <51148553+markyanjunch@users.noreply.github.com>
2022-02-06 15:55:32 -0500

    Complete ex1

--- myJS.js
+++ myJS.js
@@ -2,9 +2,11 @@
 * Excercise 1
 *
 */
-
-
-
+const color_block = document.querySelector('#color-block');
+const color_name = document.querySelector('#color-name');
+let flag = 1;
+let color_old = '#F08080';
+let color_new = '#1E90FF';
 /*
 * Then write a function that changes the text and the color inside the div
 *
@@ -12,21 +14,23 @@
 
 function changeColor(){
     //Write a condition determine what color it should be changed to
-    if(){
+    if(flag){
         //change the background color using JS
-
+        flag = 0;
+        color_block.style.backgroundColor = color_new;
         //Change the text of the color using the span id color-name
-
+        color_name.textContent = color_new;
     }
     else{
         //change the background color using JS
-
+        flag = 1;
+        color_block.style.backgroundColor = color_old;
         //Change the text of the color using the span id color-name
-
-
+        color_name.textContent = color_old;
     }
 }
 
+color_block.addEventListener('click', changeColor);
 
 /*
 * For excercise 2, you need to write an event handler for the button id "convertbtn"
